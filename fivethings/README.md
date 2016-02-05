c78d04247947c0775b68412deee207323d8be329
Jonathan Waldstein <Jpwaldstein@gmail.com>
2016-02-05 14:06:46 -0500

    Try a vue boilerplate

---
+++ fivethings/README.md
@@ -0,0 +1,24 @@
+# My Vue.js Boilerplate
+
+* vue.js
+* browserify
+  * beefy (auto build & livereload)
+  * vueify
+
+## setup
+```
+npm install
+```
+
+## start develop
+* livereload and autobuild
+  * not created bundle.js
+```
+npm run dev
+```
+
+## build
+* create bundle.js
+```
+npm run build
+```
---
+++ fivethings/src/tracks.vue
@@ -0,0 +1,28 @@
+<script lang="coffee">
+  module.exports =
+    inherit: true
+    replace: true
+</script>
+
+<template>
+  <div class="tracks">
+    <ul class="list-group">
+      <li class="list-group-item" v-repeat="tracks">
+        <span class="label label-info">{{$index+1}}</span>
+        <a href="{{url}}" target="_blank"><span class="track">{{name}}</span></a>
+        <span class="artist">{{artist.name}}</span>
+        <small class="listeners glyphicon glyphicon-headphones">{{listeners}}</small>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style lang="stylus">
+  .tracks
+    .track
+      font-size: 20px
+      margin-right: 10px
+    .artist
+      font-size: 14px
+      margin-right: 10px
+</style>
